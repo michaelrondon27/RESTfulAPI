c6012d9f12e2bc1a03654739051ffd04958387d9
michael <mrondon72@gmail.com>
2017-12-26 10:14:03 -0400

    Sesion 12 y 13
    Creanto Rutas y Controladores REST

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('fabricantes', 'FabricanteController');
+Route::resource('vehiculos', 'VehiculoController', ['only' => ['index', 'show']]);
+Route::resource('fabricantes.vehiculos', 'FabricanteVehiculoController', ['except' => ['show']]);
